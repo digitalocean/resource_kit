6ac5cdce1db8b1a78c99b60daa75a85778069b25
Ivan Vanderbyl & Robert Ross <pair+ivan+rross@digitalocean.com>
2014-08-01 16:32:37 -0700

    Include example for retrieving all digital ocean droplets.

---
+++ examples/digitalocean_droplets.rb
@@ -0,0 +1,53 @@
+require 'kartograph'
+
+class Droplet
+  attr_accessor :id, :name, :region, :size, :image
+end
+
+class DropletMapping
+  include Kartograph::DSL
+
+  kartograph do
+    mapping Droplet
+    root_key plural: 'droplets', singular: 'droplet', scopes: [:read]
+
+    property :id, scopes: [:read]
+    property :name, scopes: [:read, :create]
+    property :size, scopes: [:read, :create]
+    property :image, scopes: [:read, :create]
+    property :region, scopes: [:read, :create]
+  end
+end
+
+class DropletResource < ResourceKit::Resource
+  resources do
+    action :all do
+      verb :get
+      path '/v2/droplets'
+      handler(200) { |response| DropletMapping.extract_collection(response.body, :read) }
+    end
+
+    action :find do
+      verb :get
+      path '/v2/droplets/:id'
+      handler(200) { |response| DropletMapping.extract_single(response.body, :read) }
+    end
+
+    action :create do
+      verb :post
+      path '/v2/droplets'
+      body {|object| DropletMapping.representation_for(:create, object) }
+      handler(202) { |response| DropletMapping.extract_single(response.body, :read) }
+    end
+  end
+end
+
+token = 'YOUR_ACCESS_TOKEN'
+connection = Faraday.new(url: 'https://api.digitalocean.com', headers: { content_type: 'application/json', authorization: "Bearer #{token}" }) do |req|
+  req.adapter :net_http
+end
+
+resource = DropletResource.new(connection)
+
+# Retrieve all droplets
+puts resource.all
