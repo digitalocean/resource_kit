0649fa707d9bf6591e42b3d4f9d96190d9a354c0
Ivan Vanderbyl & Robert Ross <pair+ivan+rross@digitalocean.com>
2014-08-01 13:18:18 -0700

    RSpec describe.

--- spec/lib/resource_kit/status_code_mapper_spec.rb
+++ spec/lib/resource_kit/status_code_mapper_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe ResourceKit::StatusCodeMapper do
+RSpec.describe ResourceKit::StatusCodeMapper do
   describe '.code_for' do
     it 'returns the status code for a symbol' do
       expect(described_class.code_for(:ok)).to eq(200)
