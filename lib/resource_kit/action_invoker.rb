c3e268d02d99ea63b42122f0611e031f7f58bf59
Ivan Vanderbyl & Robert Ross <pair+ivan+rross@digitalocean.com>
2014-08-06 14:47:13 -0700

    Convert ActionInvoker to an instance an delegate .call to it.

--- lib/resource_kit/action_invoker.rb
+++ lib/resource_kit/action_invoker.rb
@@ -1,23 +1,19 @@
 module ResourceKit
   class ActionInvoker
-    def self.call(action, connection, *args)
-      raise ArgumentError, "Verb '#{action.verb}' is not allowed" unless action.verb.in?(ALLOWED_VERBS)
-      options = args.last.kind_of?(Hash) ? args.last : {}
-      resolver = EndpointResolver.new(path: action.path, query_param_keys: action.query_keys)
+    attr_reader :action, :connection, :args, :options
 
-      if action.body and action.verb.in?([:post, :put, :patch])
-        # This request is going to have a response body. Handle it.
-        response = connection.send(action.verb, resolver.resolve(options)) do |request|
-          request.body = construct_body(*args, action)
-        end
-      else
-        response = connection.send(action.verb, resolver.resolve(options))
-      end
-
-      handle_response(response, action)
+    def initialize(action, connection, *args)
+      @action = action
+      @connection = connection
+      @args = args
+      @options = args.last.kind_of?(Hash) ? args.last : {}
     end
 
-    def self.handle_response(response, action)
+    def self.call(action, connection, *args)
+      new(action, connection, *args).handle_response
+    end
+
+    def handle_response
       if handler = action.handlers[response.status]
         handler.call(response)
       else
@@ -25,8 +21,27 @@ module ResourceKit
       end
     end
 
-    def self.construct_body(*args, action)
+    def construct_body
       action.body.call(*args[0..(action.body.arity - 1)])
+    end
+
+    def response
+      return @response if @response
+
+      raise ArgumentError, "Verb '#{action.verb}' is not allowed" unless action.verb.in?(ALLOWED_VERBS)
+
+      if action.body and action.verb.in?([:post, :put, :patch])
+        # This request is going to have a response body. Handle it.
+        @response = connection.send(action.verb, resolver.resolve(options)) do |request|
+          request.body = construct_body
+        end
+      else
+        @response = connection.send(action.verb, resolver.resolve(options))
+      end
+    end
+
+    def resolver
+      EndpointResolver.new(path: action.path, query_param_keys: action.query_keys)
     end
   end
 end
