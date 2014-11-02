21a426dc4f61e3a15cb4489d6a3abc89d3628915
Robert Ross <rross@digitalocean.com>
2014-11-02 18:30:47 -0500

    Ensure that we inherit our resources.

---
+++ spec/integration/inheritence_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+class DummyInheritenceResource < ResourceKit::Resource
+  resources do
+    default_handler(:ok) { |resp| "Hello" }
+    get "/dummy" => :dummy
+  end
+end
+
+class DummyChildResource < DummyInheritenceResource
+  resources do
+    get "/inherited" => :inherited
+  end
+end
+
+RSpec.describe 'Resource Inheritence' do
+  it 'inherits the superclasses actions' do
+    action = DummyChildResource.resources.find_action(:dummy)
+    expect(action).to_not be_nil
+    expect(action.name).to eq(:dummy)
+  end
+
+  it 'inherits default handlers' do
+    expect(DummyChildResource.resources.default_handlers[200]).to_not be_nil
+  end
+end
